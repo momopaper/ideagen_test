20e982b47326cab40a031f0b373a54c71716c2d9
momopaper <lee83_@hotmail.com>
2024-06-07 17:08:58 +0800

    update
    - added livewire config file

---
+++ config/livewire.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'livewire_update_url' => env('LIVEWIRE_UPDATE_PATH', 'livewire/update'),
+    'livewire_javascript_url' => env('LIVEWIRE_JAVASCRIPT_PATH', 'livewire/livewire.js'),
+];
